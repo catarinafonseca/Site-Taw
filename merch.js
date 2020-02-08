17a300d3aeb5b534b1611c06c9ce107a9a4622c6
catarinafonseca <catarinaconstantef@gmail.com>
2020-02-08 01:18:55 +0000

    ok

--- merch.js
+++ merch.js
@@ -1,11 +1,57 @@
 let carts=document.querySelectorAll('.add-to-cart');
 
-let prodcts={
+let products=[
     {
         name:'T-shirt Tell Me You Love Me',
-        
+        tag:'tshirtlove',
+        price:99.99,
+        inCart:0,        
+    },
+    {
+        name:'T-shirt Sorry Not Sorry + Digital Album',
+        tag:'tshirtsorry',
+        price:150.00,
+        inCart:0, 
+    },
+    {
+        name:'Tour Hoddie',
+        tag:'tourhoddie',
+        price:200.00,
+        inCart:0, 
+    },
+    {
+        name:'Demi Sweatshirt',
+        tag:'demisweat',
+        price:69.99,
+        inCart:0, 
+    },
+    {
+        name:'Tell Me You Love Me Bomber Jacket',
+        tag:'jacket',
+        price:250.00,
+        inCart:0, 
+    },
+    {
+        name:'Sorry not Sorry Hat + Digital Album',
+        tag:'hatsorry',
+        price:150.00,
+        inCart:0, 
+    },
+    {
+        name:'Demi Beanie',
+        tag:'beanie',
+        price:60.00,
+        inCart:0, 
+    },
+    {
+        name:'Confident Mug',
+        tag:'mug',
+        price:49.99,
+        inCart:0, 
     }
-}
+
+
+]
 
 
 for(let i=0; i<carts.length;i++){
@@ -16,6 +62,14 @@ for(let i=0; i<carts.length;i++){
 
 function cartNumbers(){
     let prodNumbers=localStorage.getItem('cartNumbers');
+
     prodNumbers=parseInt(prodNumbers);
-    localStorage.setItem('cartNumbers',1);
+
+    if(prodNumbers){
+        localStorage.setItem('cartNumbers',prodNumbers + 1);
+        document.querySelector('.btn span').textContent= (prodNumbers + 1);
+    }else{
+        localStorage.setItem('cartNumbers',1);
+        document.querySelector('.btn span').textContent=1;
+    }
 }
